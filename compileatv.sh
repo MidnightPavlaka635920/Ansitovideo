fe8af763230832d21c25080a9691985f8249286f
MidnightPavlaka <156126701+MidnightPavlaka635920@users.noreply.github.com>
2025-10-06 16:20:22 +0200

    Update compileatv.sh

--- compileatv.sh
+++ compileatv.sh
@@ -1,3 +1,3 @@
-g++ ansitovideo.cpp -o ansitovideo \
-    -L/usr/local/lib -lavformat -lavcodec -lswscale -lavutil -lz -lm -lpthread
-
+ffmpeg_flags=$(pkg-config --cflags --libs libavformat libavcodec libswscale libavutil 2>/dev/null)
+freetype_flags=$(pkg-config --cflags --libs freetype2 2>/dev/null)
+g++ ansitovideo.cpp -o ansitovideo $ffmpeg_flags $freetype_flags
